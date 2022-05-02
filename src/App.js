ac05185cad40cc046751a4de050be930edcb54d3
Shahriar Kasem <shahriarkasem@gmail.com>
2022-05-02 22:48:16 +0600

    notfound/404 page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/pages/Home/Home/Home';
 import Header from './components/shared/Header/Header';
 import { Route, Routes } from 'react-router-dom';
+import NotFound from './components/shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
