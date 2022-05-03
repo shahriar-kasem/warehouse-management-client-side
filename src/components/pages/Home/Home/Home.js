89b5f2b66575bc9319ee2cf6eae4a2cbd667f0f6
Shahriar Kasem <shahriarkasem@gmail.com>
2022-05-03 21:23:46 +0600

    responsive navbar added

--- src/components/pages/Home/Home/Home.js
+++ src/components/pages/Home/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from '../../../shared/Navbar/Navbar';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home</h1>
+            <Navbar></Navbar>
             <Banner></Banner>
         </div>
     );
---
+++ src/components/shared/Navbar/Navbar.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    return (
+        <div>
+            <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-rose-500">
+                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                        <Link
+                            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                            to="#pablo"
+                        >
+                            Warehouse Management
+                        </Link>
+                        <button
+                            className="text-white bg-red-900 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                            type="button"
+                            onClick={() => setNavbarOpen(!navbarOpen)}
+                        >
+                        </button>
+                    </div>
+                    <div
+                        className={
+                            "lg:flex flex-grow items-center" +
+                            (navbarOpen ? " flex" : " hidden")
+                        }
+                        id="example-navbar-danger"
+                    >
+                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                            <li className="nav-item">
+                                <Link
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    to="#pablo"
+                                >
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    to="#pablo"
+                                >
+                                    Inventory
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    to="#pablo"
+                                >
+                                    Manage Inventory
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    to="#pablo"
+                                >
+                                    Add a new item
+                                </Link>
+                            </li><li className="nav-item">
+                                <Link
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    to="#pablo"
+                                >
+                                    Log in
+                                </Link>
+                            </li><li className="nav-item">
+                                <Link
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    to="#pablo"
+                                >
+                                    Log out
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
