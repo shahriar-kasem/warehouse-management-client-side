669c57312b57f512b79972f602f35ca98b3c96fa
Shahriar Kasem <shahriarkasem@gmail.com>
2022-05-05 20:23:50 +0600

    minor style updated

--- src/components/pages/Home/Service/Service.js
+++ src/components/pages/Home/Service/Service.js
@@ -5,11 +5,11 @@ const Service = ({service, getServiceDetail}) => {
     // console.log(service)
     const shortDescription = description.slice(0, 200);
     return (
-        <div className='border-2 border-blue-200 rounded px-2'>
+        <div className='border-2 border-blue-200 rounded px-2 lg:px-5'>
             <h1 className='font-semibold text-blue-400 text-lg'>{name}</h1>
             <p><span className='font-semibold text-blue-800'>Details</span>: {shortDescription}</p>
-          <div className='my-1 bg-rose-400 hover:bg-rose-500 rounded lg:mx-24 md:mx-10 mx-5'>
-          <button className='px-2 py-1 mt-1 text-white mb-2 mx-1' onClick={() => getServiceDetail(_id)}>See Details</button>
+          <div className='my-1 rounded lg:mx-24 md:mx-10 mx-5'>
+          <button className='bg-rose-400 px-5 py-1 mt-1 rounded hover:bg-rose-500 text-white mb-2 mx-5' onClick={() => getServiceDetail(_id)}>See Details</button>
           </div>
         </div>
     );
