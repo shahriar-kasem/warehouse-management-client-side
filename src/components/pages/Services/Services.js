19757ef4aad2d67841e4df731fd95be01f415d63
Shahriar Kasem <shahriarkasem@gmail.com>
2022-05-08 11:46:05 +0600

    bug fixed

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -17,7 +17,7 @@ const Services = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 md:mx-5 lg:mx-10 mx-1'>
                 {
                     services.map(service => <Service
-                    key={service}
+                    key={service._id}
                     getServiceDetail={getServiceDetail}
                     service={service}
                     ></Service>)
